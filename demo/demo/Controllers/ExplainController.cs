dd4ba2986ec15eef8d0fbf77be56110d6e534a79
1529464062· <1529464062@qq.com>
2017-03-15 20:10:09 +0800

    适应性修改

---
+++ demo/demo/Controllers/ExplainController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace demo.Controllers
+{
+    public class ExplainController : Controller
+    {
+        public ActionResult Major()
+        {
+            ViewData["list"] = "[[123,234],[345,456]]";
+            return View();
+        }
+    }
+}
